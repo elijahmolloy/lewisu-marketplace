a26ad8994b248873a3c5eefaae73260fc7bdd631
Elijah Molloy <elijahmolloy@gmail.com>
2023-06-03 19:29:35 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.enableVersioning({
+		type: VersioningType.URI
+	});
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true,
+			transform: true
+		})
+	);
 
 	const config = new DocumentBuilder()
 		.setTitle('LewisU Marketplace API')
 		.setVersion('0.0.1')
+		.addBearerAuth()
 		.build();
 
 	const document = SwaggerModule.createDocument(app, config);
