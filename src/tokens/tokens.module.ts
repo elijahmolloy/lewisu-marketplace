2231a2e73cbad1f9c95384de5bc02de3874ce9cd
Elijah Molloy <elijahmolloy@gmail.com>
2023-06-03 20:28:56 -0600

    Updates to tokens.module and users.module. Update to users.controllers params

--- src/tokens/tokens.module.ts
+++ src/tokens/tokens.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TokensService } from './tokens.service';
+import { UsersModule } from 'src/users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { Token, TokenSchema } from './entities/token.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-	providers: [TokensService]
+	imports: [
+		MongooseModule.forFeature([{ name: Token.name, schema: TokenSchema }]),
+		ConfigModule,
+		UsersModule,
+		JwtModule.register({
+			secret: process.env.JWT_SECRET
+		})
+	],
+	providers: [TokensService],
+	exports: [TokensService]
 })
 export class TokensModule {}
