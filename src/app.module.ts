097fcf5327b03d277ea3234144b17e70e0ba899d
Elijah Molloy <elijahmolloy@gmail.com>
2023-06-03 18:51:50 -0600

    Creation of tokens module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { TokensModule } from './tokens/tokens.module';
 
 @Module({
-	imports: [ConfigModule.forRoot(), AuthModule, UsersModule],
+	imports: [ConfigModule.forRoot(), AuthModule, UsersModule, TokensModule],
 	controllers: [],
 	providers: []
 })
